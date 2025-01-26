8839a5c8cbaa7c278093898b5105283f78b284cc
Karim4691 <kalami2003@gmail.com>
2025-01-26 11:47:42 -0500

    Main

--- src/mlfq.py
+++ src/mlfq.py
@@ -17,14 +17,80 @@ class MLFQ:
     def add_task_incoming(self, new_task:Task):
         queue_nb = self.update_task_priority(new_task)
         self.PRIORITY_QUEUES[queue_nb].appendleft(new_task)
-
         database.store_task_to_database(self.database, new_task)
+        self.schedule()
+
+    def add_to_ongoing(self, task):
+        max_manpower_pts = team.max_manpower_pts
+        total_manpower = 0 #how much manpower added so far
+        for _,dev in team.devs.items()
+            if (dev.current_task != None) : 
+                continue
+            if (dev.experience_level + total_manpower > manpower_pts):
+            continue
+        
+            dev.current_task = task
+            dev.task_start_time = datetime.datetime.now()
+            task.attributed_devs.append(dev)
+            team.manpower_pts -= dev.experience_level
+            ONGOING_TASKS.append(task)
+            change_status_to_ongoing(task)
+            return True
+        return False
+    
+    def add_to_full_ongoing(self, task):
+        if (len(ONGOING_TASKS) == 0) :
+            return false
+        least_priority_task = ONGOING_TASKS[0]
+        for task in ONGOING_TASKS.items():
+            if (task.priority > least_priority_task):
+                least_priority_task = task
+        
+        if (least_priority_task.priority <= task.priority): #ongoing has more priority
+            return False 
+
+        #replace tasks
+        for (i in range(len(least_priority_task.attributed_devs))):
+            least_priority_task.estimated_time -= nb_seconds_delta(task.task_start_time)
+            dev.current_task = None
+            dev.task_start_time = None
+            team.manpower_pts += dev.experience_level
+        
+        ONGOING_TASKS.remove(least_priority_task)
+        if(least_priority_task.estimated_time > 0) : 
+            add_task_incoming(least_priority_task) #add it back to queue
+            change_status_to_incomplete(least_priority_task)
+        else : 
+            change_status_to_complete(least_priority_task)
+        add_to_ongoing(task)
+        task.change_status_to_ongoing(task)
+        return True
+        
+
+    def schedule(self):
+        manpower_pts = team.manpower_pts
+        max_manpower_pts = team.max_manpower_pts
+        for i in range(1,5):
+            if (len(self.PRIORITY_QUEUES[i]) != 0):
+                task = self.PRIORITY_QUEUES[i].popleft()
+                if (manpower_pts > 0) : # set workers to task
+                    if (add_to_ongoing(task) == False) :
+                        self.PRIORITY_QUEUES[i].appendleft(task) 
+                else :
+                    if (add_to_full_ongoing(task) == False) :
+                        self.PRIORITY_QUEUES[i].appendleft(task)
             
 
     def nb_hours_until_deadline(self, deadline) :
         now = datetime.datetime.now()
         return ((deadline.year - now.year) * 8760 +
         (deadline.month - now.month) * 730 + (deadline.day - now.day) * 24)
+
+    def nb_seconds_delta(self, time) : #delta difference with respect to now
+        now = datetime.datetime.now()
+        return ((now.year - time.year) * 31536000 +
+        (now.month - time.month) * 2628288 + (now.day - time.day) * 86400 +
+        (now.minute - time.minute) * 60 - (now.minute - time.minute))
 
     def get_delta_time_value(self, task:Task):
         """
@@ -86,6 +152,8 @@ class MLFQ:
                     else :
                         # If the task was seen before in the update, don't check it twice.
                         pass
+    
+    
 
     def change_status_to_ongoing(self, task:Task):
         database.update_task(self.database, task, Status.ONGOING)
