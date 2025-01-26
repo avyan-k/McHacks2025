6e1bbd7304cd6716e5784b8c8dea8f5fa36bdfbe
Karim4691 <kalami2003@gmail.com>
2025-01-25 21:53:24 -0500

    mlfq

--- src/mlfq.py
+++ src/mlfq.py
@@ -1,2 +1,17 @@
 from collections import deque
+import datetime
+import Task
 
+def mlfq():
+    def __init__(self, dev_power, max_dev_power):
+        self.PRIORITY_QUEUE1 = deque()
+        self.PRIORITY_QUEUE2 = deque()
+        self.PRIORITY_QUEUE3 = deque()
+        self.PRIORITY_QUEUE4 = deque()
+        self.PRIORITY_QUEUE5 = deque()
+        self.dev_power = dev_power
+        self.max_dev_power = max_dev_power
+
+def add_task(new_task:Task):
+    now = datetime.datetime.now()
+    nb_hours_until_deadline = 
